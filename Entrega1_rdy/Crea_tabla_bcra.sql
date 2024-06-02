0bcc15c63cc9e29458b07556a8ef61c9f8bb214b
MarcosCervera <you@example.com>
2024-06-02 18:57:42 -0300

    entrega

--- Entrega1_rdy/Crea_tabla_bcra.sql
+++ Entrega1_rdy/Crea_tabla_bcra.sql
@@ -11,6 +11,32 @@ CREATE TABLE stage_bcra(
 );
 
 
+DROP TABLE IF EXISTS marcoscervera_coderhouse.stage_bcra_full;
+
+CREATE TABLE stage_bcra_full(
+	fecha Date,
+	base VARCHAR(50),
+	cajas_ahorro VARCHAR(50),
+	cer VARCHAR(50),
+	circulacion_monetaria VARCHAR(50),
+	cuentas_corrientes VARCHAR(50),
+	depositos VARCHAR(50),
+	inflacion_interanual_oficial VARCHAR(50),
+	inflacion_mensual_oficial VARCHAR(50),
+	plazo_fijo VARCHAR(50),
+	reservas VARCHAR(50),
+	usd VARCHAR(50),
+	usd_of VARCHAR(50),
+	usd_of_minorista VARCHAR(50),
+	uva VARCHAR(50)
+);
+
+
+select *
+from stage_bcra_full
+
+
+
 SELECT  
 *
 FROM marcoscervera_coderhouse.stage_bcra
